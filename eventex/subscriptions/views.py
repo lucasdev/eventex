d087d188e3da09624fed611f4f5c5d6a7600b98f
Lucas <contato@lucass.com.br>
2019-08-18 11:06:10 -0300

    Realizando inscrição com envio de e-mail

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,9 +1,27 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
-# Create your views here.
 from eventex.subscriptions.forms import SubscriptionsForm
 
 
 def subscribe(request):
-    form = {'form': SubscriptionsForm()}
-    return render(request, 'subscription_form.html', form)
+    if request.method == 'POST':
+        form = SubscriptionsForm(request.POST)
+        if form.is_valid():
+            body = render_to_string('subscription_mail.txt', form.cleaned_data)
+            mail.send_mail('Confirmação de Inscrição',
+                           body,
+                           'contato@eventex.com.br',
+                           [form.cleaned_data['email']])
+
+            messages.success(request, 'Inscrição realizada com sucesso')
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscription_form.html', {'form': form})
+
+    else:
+        form = {'form': SubscriptionsForm()}
+        return render(request, 'subscription_form.html', form)
