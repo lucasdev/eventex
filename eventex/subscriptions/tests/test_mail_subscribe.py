0807f2bec4b249ad50ea2dd80233794c9001b973
Lucas <contato@lucass.com.br>
2019-10-05 02:06:13 -0300

    refatora teste

--- eventex/subscriptions/tests/test_mail_subscribe.py
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.core import mail
+from django.shortcuts import resolve_url as r
 from django.test import TestCase
 
 
@@ -8,7 +9,7 @@ class SubscribePostValid(TestCase):
     def setUp(self):
         self.data = dict(name='Lucas', cpf='12345678901', email='contato@lucass.com.br',
                          phone='61-99210-0606', uuid=str(uuid.uuid3(uuid.NAMESPACE_DNS, '12345678901')))
-        self.client.post('/inscricao/', self.data)
+        self.client.post(r('subscriptions:new'), self.data)
         self.mail = mail.outbox[0]
 
     def test_subscrition_email_subject(self):
